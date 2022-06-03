be192580f5222aea9444e1804d01bb112b21c021
Sergio Gonzalez <s.gonzalez@codeur.online>
2022-06-03 11:48:39 +0200

    a propos et competences ~ terminés, début projets

--- js/main.js
+++ js/main.js
@@ -50,3 +50,85 @@ setInterval(function(){
     update();
 },1000/fps);
 
+/*rangement menu*/
+
+let rangementMenuCheck = document.getElementById('mon-menu-burger');
+let rangementMenu = document.getElementsByClassName('rangement-menu');
+let zoneHaute = document.getElementsByClassName('zoneHaute');
+
+window.addEventListener('scroll', ()=>{
+    rangementMenuCheck.checked = true;
+    rangementMenu[0].style.opacity = '0';
+    zoneHaute[0].style.height = '200px';
+    zoneHaute[0].style.maxHeight = '200px';
+})
+
+zoneHaute[0].addEventListener('mouseover', ()=>{
+    rangementMenu[0].style.opacity = '1';
+    zoneHaute[0].style.maxHeight = '0';
+})
+
+rangementMenu[0].addEventListener('mouseleave', ()=>{
+    rangementMenu[0].style.opacity = '0';
+    zoneHaute[0].style.height = '200px';
+    zoneHaute[0].style.maxHeight = '200px';
+})
+
+/*parallax*/
+
+let parallax = document.getElementsByClassName('parallax');
+let xStartingPositionParallax = [];
+let xPositionParallax = [];
+for(let j=0; j<parallax.length; j++){
+    xPositionParallax.push(parallax[j].offsetTop);
+    xStartingPositionParallax.push(parallax[j].style.top);
+}
+
+xPositionParallax[0] = xStartingPositionParallax[0] - window.scrollY * 2.5;
+    xPositionParallax[1] = xStartingPositionParallax[1] - window.scrollY / 2;
+    parallax[0].style.top = xPositionParallax[0] + 'px';
+    parallax[1].style.top = xPositionParallax[1] + 'px';
+
+window.addEventListener('scroll', ()=>{    
+    xPositionParallax[0] = xStartingPositionParallax[0] - window.scrollY * 2.5;
+    xPositionParallax[1] = xStartingPositionParallax[1] - window.scrollY / 2;
+    parallax[0].style.top = xPositionParallax[0] + 'px';
+    parallax[1].style.top = xPositionParallax[1] + 'px';
+})
+
+/*Opacity section Mes Compétences */
+
+let mesCompetences = document.getElementsByClassName('mesCompetences')[0];
+let categoriesCompetences = document.getElementsByClassName('categorieCompetences');
+let k;
+
+if(window.innerHeight + parallax[1].clientHeight + xStartingPositionParallax[1] - window.scrollY / 2 < window.scrollY){
+    for(k = 0; k <categoriesCompetences.length; k++){
+        categoriesCompetences[k].style.opacity = 1;
+        categoriesCompetences[k].style.transitionDelay = (k+1) +'s';
+    }
+    mesCompetences.style.opacity = 1;
+}else{
+    for(k = 0; k <categoriesCompetences.length; k++){
+        categoriesCompetences[k].style.opacity = 0;
+        categoriesCompetences[k].style.transitionDelay = 0 + 's';
+    }
+    mesCompetences.style.opacity = 0;
+}
+
+window.addEventListener('scroll', ()=>{
+    if(window.innerHeight + parallax[1].clientHeight + xStartingPositionParallax[1] - window.scrollY / 2 < window.scrollY){
+        for(k = 0; k <categoriesCompetences.length; k++){
+            categoriesCompetences[k].style.opacity = 1;
+            categoriesCompetences[k].style.transitionDelay = (k+1) +'s';
+        }
+        mesCompetences.style.opacity = 1;
+    }else{
+        for(k = 0; k <categoriesCompetences.length; k++){
+            categoriesCompetences[k].style.opacity = 0;
+            categoriesCompetences[k].style.transitionDelay = 0 + 's';
+        }
+        mesCompetences.style.opacity = 0;
+    }
+})
+
